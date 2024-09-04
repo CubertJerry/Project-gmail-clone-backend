d55ba3d20df8cecef844630eaaaa72c23a0dbf53
CubertJerry <140590249+CubertJerry@users.noreply.github.com>
2024-09-04 22:52:47 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -8,7 +8,9 @@ require('dotenv').config();  // Add this line
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
-
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 app.use(cors());
 app.use(express.json());
 
