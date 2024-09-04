07a7fd7e882dbdab6a1867f8800f6b7fdec39872
CubertJerry <140590249+CubertJerry@users.noreply.github.com>
2024-09-04 22:41:58 +0530

    Update user.js

--- user.js
+++ user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });const UserSchema = new mongoose.Schema({
 const UserSchema = new mongoose.Schema({
   email: {
     type: String,
